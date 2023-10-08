859587bdfe0740754df2c5237b66f6ca0b515da8
nisha-nt1 <118256597+nisha-nt1@users.noreply.github.com>
2023-10-08 11:22:23 +0530

    Add files via upload

---
+++ ArrayShuffle.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayShuffle {
+    public static void main(String[] args) {
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7};
+        List<Integer> list = Arrays.asList(array);
+        
+        // Shuffle the list
+        Collections.shuffle(list);
+        
+        System.out.println("Shuffled array: " + Arrays.toString(list.toArray()));
+    }
+}
